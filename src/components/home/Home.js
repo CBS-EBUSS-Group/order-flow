faab7b9a0f48ee73016b19cb3093295e10dec1f4
Dominik Gadesmann <dominik.gadesmann@gmail.com>
2020-11-21 19:09:20 +0100

    initial file structure and example

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskBar from '../taskBar';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+      <TaskBar/>
+    </div>
+  );
+}
+
+export default Home
---
+++ src/components/taskBar/TaskBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TaskBar = () => {
+  return <div>TaskBar</div>
+}
+
+export default TaskBar;
