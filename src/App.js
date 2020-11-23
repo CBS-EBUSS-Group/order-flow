220cef735f8581df99673d0c0078a191dcc2d805
Dominik Gadesmann <dominik.gadesmann@gmail.com>
2020-11-23 19:41:39 +0100

    adding home styles

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           </Switch>
           <TaskBar />
         </div>
-        {/*<ChatBot />*/}
+        <ChatBot />
       </div>
     </Router>
   );
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styles from "./Home.module.css";
-import { Button } from "react-bootstrap";
 
 const Home = () => {
+  const { balance } = useSelector((state) => state.account);
   return (
-    <div className={styles.accountArea}>
-      <h1>Account Area</h1>
-      <p>Depot Nr: </p>
-      <p>Buying Power: </p>
-      <Button variant="light" className={styles.but1}>
-        BUY
-      </Button>
-      <Button variant="light">SELL</Button>
-      <Button variant="light">PORTFOLIO</Button>
-      <Button variant="light">TAX REPORT</Button>
+    <div className={`page ${styles.container}`}>
+      <div className={styles.innerBox}>
+        <h1>Your Account Overview</h1>
+        <p>Depot Nr: 12663390</p>
+        <p>Buying Power: {balance} EUR</p>
+        <div className={styles.linkContainer}>
+          <Link to="/market" className={styles.buttonLink}>
+            Buy
+          </Link>
+          <Link to="/depot" className={styles.buttonLink}>
+            Depot
+          </Link>
+          <Link to="/account" className={styles.buttonLink}>
+            Account
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
